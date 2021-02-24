0519e7bbfa162f69298a86ae81e7b0af563165f2
João Reis <joao@reis.Home>
2021-02-24 17:14:35 +0000

    project

---
+++ A03/do_all.bash
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+ulimit -s 16384
+for n in {100..3}; do
+  if [ -e stop_request ]; then
+    exit 0
+  fi
+  f=$(printf %04d $n)
+  export TIMEFORMAT="$n done in %3Us"
+  if [ ! -e $f ]; then
+    # time ./A03 -x $n >$f
+    time ./A03 -x $f >> 'output.txt'
+  fi
+done
+echo All done
